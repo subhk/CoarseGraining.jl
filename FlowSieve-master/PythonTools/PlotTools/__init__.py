d7dac598e5fcdcd48606ba6011f3cdac005158f8
Subhajit Kar <subhajitkar19@gmail.com>
2025-08-13 03:56:14 +0300

    updates

--- FlowSieve-master/PythonTools/PlotTools/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from .ScientificCbar import ScientificCbar
-from .merge_to_mp4 import merge_to_mp4
-from .AddParallels_and_Meridians import AddParallels_and_Meridians
-from .LabelTimeAxis import LabelTimeAxis
-from .getAreas import getAreas
-
-dpi=300
--- FlowSieve-master/Tests/README.md
+++
@@ -1,3 +0,0 @@
-## Tests
-
-**NOTE** The testing framework is currently obsolete / non-functional. Please use the Tutorials to verify that FlowSieve is working as expected.
